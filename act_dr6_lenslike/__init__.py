ef7aefca498694f2fcfa1ba05375a8f481e72def
Ian Harrison <itrharrison@gmail.com>
2023-10-11 15:43:22 +0100

    version bump

--- act_dr6_lenslike/__init__.py
+++ act_dr6_lenslike/__init__.py
@@ -1,5 +1,5 @@
 """Likelihood for the Atacama Cosmology Telescope DR6 CMB lensing data."""
 __author__ = ["Mathew Madhavacheril", "Ian Harrison"]
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .act_dr6_lenslike import *
