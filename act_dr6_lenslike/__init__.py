22f1863c0d14d3dbc90da7eb62a9232742fa0229
Mathew Madhavacheril <mathewsyriac@gmail.com>
2024-02-05 19:25:50 -0500

    version update

--- act_dr6_lenslike/__init__.py
+++ act_dr6_lenslike/__init__.py
@@ -1,5 +1,5 @@
 """Likelihood for the Atacama Cosmology Telescope DR6 CMB lensing data."""
 __author__ = ["Mathew Madhavacheril", "Ian Harrison"]
-__version__ = "1.0.6"
+__version__ = "1.2.0"
 
 from .act_dr6_lenslike import *
